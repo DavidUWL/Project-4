d6ca41e3c8207860b31f3320eb9cb94627222315
David Kirby <davidkirbymusician@gmail.com>
2023-09-25 18:52:38 +0000

    changed time field in form to be a dropdown choice instead of scroll wheel.

--- therestaurant/forms.py
+++ therestaurant/forms.py
@@ -11,8 +11,29 @@ class ReserveForm(ModelForm):
         (4, '4'),
     ]
 
+    TIME_CHOICES = [
+        (17.00, '17.00'),
+        (17.30, '17.30'),
+        (18.00, '18.00'),
+        (18.30, '18.30'),
+        (19.00, '19.00'),
+        (19.30, '19.30'),
+        (20.00, '20.00'),
+        (20.30, '20.30'),
+        (21.00, '21.00'),
+        (21.30, '21.30'),
+        (22.00, '22.00'),
+        (22.30, '22.30'),
+        (23.00, '23.00'),
+    ]
+
     covers = forms.ChoiceField(
         choices=COVERS_CHOICES,
+        widget=forms.Select(attrs={'class': 'form-control', 'required': True}),
+    )
+
+    time = forms.ChoiceField(
+        choices=TIME_CHOICES,
         widget=forms.Select(attrs={'class': 'form-control', 'required': True}),
     )
 
@@ -26,7 +47,7 @@ class ReserveForm(ModelForm):
             'email': forms.EmailInput(attrs={'class': 'form-control', "required": True}),
             'contact_number': forms.NumberInput(attrs={'type': 'tel', 'class': 'form-control', "required": True}),
             'date': forms.DateInput(attrs={'type': 'date', 'class': 'form-control', "required": True}),
-            'time': forms.TimeInput(attrs={'type': 'time', 'class': 'form-control', "required": True}),
+            # 'time': forms.TimeInput(attrs={'type': 'time', 'class': 'form-control', "required": True}),
             # 'covers': forms.Select(attrs={'class': 'form-control'}),
         }
 
