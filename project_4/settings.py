81f399ab52a928fa306403c24a487be3052cf538
David Kirby <davidkirbymusician@gmail.com>
2023-11-18 11:36:58 +0000

    Developement disabled.

--- project_4/settings.py
+++ project_4/settings.py
@@ -29,7 +29,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if DEBUG:
     ALLOWED_HOSTS = ['8000-daviduwl-project4-jwzitk3c01w.ws-eu106.gitpod.io']
