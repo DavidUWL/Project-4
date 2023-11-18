5b3bc7ad1e9b28724a5e86592c5b0282803b69d1
David Kirby <davidkirbymusician@gmail.com>
2023-11-18 13:49:47 +0000

    developement debugging for 500 error.

--- project_4/settings.py
+++ project_4/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = False
+DEBUG = True
 
 if DEBUG:
     ALLOWED_HOSTS = ['8000-daviduwl-project4-jwzitk3c01w.ws-eu106.gitpod.io']
