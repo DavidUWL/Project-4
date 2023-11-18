60852f88f7aeab4312bfb2e7fef12dd37d5f1c56
David Kirby <davidkirbymusician@gmail.com>
2023-11-18 13:56:35 +0000

    modified contact_number field in model.

--- project_4/settings.py
+++ project_4/settings.py
@@ -28,7 +28,7 @@ TEMPLATES_DIR = os.path.join(BASE_DIR, 'templates')
 SECRET_KEY = config('SECRET_KEY')
 
 # SECURITY WARNING: don't run with debug turned on in production!
-DEBUG = True
+DEBUG = False
 
 if DEBUG:
     ALLOWED_HOSTS = ['8000-daviduwl-project4-jwzitk3c01w.ws-eu106.gitpod.io']
@@ -108,7 +108,7 @@ if DEBUG:
     }
 else: 
     DATABASES = {
-        'default': dj_database_url.parse(os.environ.get('DATABASE_URL'))
+        'default': dj_database_url.parse(config('DATABASE_URL'))
     }
 
 
--- therestaurant/models.py
+++ therestaurant/models.py
@@ -23,7 +23,7 @@ class Reservation(models.Model):
     first_name = models.CharField(max_length=50, null=False, blank=False)
     last_name = models.CharField(max_length=50, null=False, blank=False)
     email = models.EmailField(null=False, blank=False)
-    contact_number = models.CharField(null=False, blank=False)
+    contact_number = models.CharField(max_length=15, null=False, blank=False)
     date = models.DateField(null=False, blank=False)
     time = models.TimeField(null=False, blank=False)
     covers = models.IntegerField(null=False, blank=False)
